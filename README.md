bb6ae40a6e5d3afb4a4e10521a83746a576694a7
lordevito <lollo.devito@hotmail.com>
2023-11-09 17:39:13 +0100

    feat: esercizio completo mail

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 Esercizio Mail:
 Inizio a creare e dichiarare un array con tutte le mail da controllare, poi creo il prompt per chiedere l'inserimento della mail da cercare.
 Creo un ciclo For per controllare che la mail inserita sia nell'array.
-Se la mail è presente scrivere in console ("Mail inserita correttamente"),
+Creo un if affinchè: se la mail è presente scrivere in console ("Mail inserita correttamente"),
 altrimenti scrivere ("Mail non riconosciuta").
--- mail/main.js
+++ mail/main.js
@@ -1,1 +1,30 @@
 `use strict`;
+
+/*Inizio a creare e dichiarare un array 
+con tutte le mail da controllare, poi creo 
+il prompt per chiedere l'inserimento della mail da cercare.*/
+
+const listaMail = [`ciro.immobile@lazio.it` , `daichi.kamada@lazio.it`, `mattia.zaccagni@lazio.it` , `ivan.provedel@lazio.it`];
+const controlMail = prompt(`Inserisci la tua mail`);
+
+/*Creo una variabile per il controllo*/
+
+let mail = false;
+
+/*Creo un ciclo For per controllare che la mail inserita sia nell'array.*/
+
+for (let i = 0; i < listaMail.length; i++){
+    let lista = listaMail[i];
+    if(lista == controlMail){
+        mail = true;
+      }
+}
+
+/*Creo un if affinchè: Se la mail è presente scrivere in console ("Mail inserita correttamente"),
+altrimenti scrivere ("Mail non riconosciuta").*/
+
+if (mail == true){
+    console.log ('Mail inserita correttamente');
+  }else{
+    console.log ('Mail non riconosciuta');
+  }
